247e4518b7b329d7e5cdaa3d9b7b2f9a60af3819
Peter van Ooy <pevanooy@gmail.com>
2017-09-18 21:37:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,6 @@ function addElementToBeginningOfArray(array, element){
   return array;
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-
+  array.unshift(element);
+  return array;
 }
