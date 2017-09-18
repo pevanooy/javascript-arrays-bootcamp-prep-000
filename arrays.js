cae6ead725797ba4d1bb95257272d7f9f3b9eb05
Peter van Ooy <pevanooy@gmail.com>
2017-09-18 21:42:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,11 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
+function addElementToEndOfArray(array, element){
+  array = [...array, element]
+  return array;  
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;  
+}
